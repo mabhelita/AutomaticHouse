cff0bf5c9b83c367b8fc4552724b39f5a5d823c1
sandraapaza <sandraapazaroman@gmail.com>
2016-07-19 23:56:50 -0500

    codigo que muestra los datos capturados por el LDR

--- temperatura/temp/temp.ino
+++ temperatura/temp/temp.ino
@@ -2,11 +2,23 @@ float tempC;
 int tempPin=0;
 int calefaccion=12;
 int ventilador=13;
+// pin donde se conectan loa leds para el fotoresistor
+int pinLed1= 2;
+int pinLed2= 3;
+// pin de entrada para el LDR
+int pinLDR=1;
+// variable para almacenar valor del LDR
+int valorLDR=0;
 void setup()
 {
   Serial.begin(9600);
   pinMode(calefaccion,OUTPUT);
   pinMode(ventilador,OUTPUT);
+
+  // configuramos los pines de salidas del led para el LDR
+  pinMode(pinLed1,OUTPUT);
+  pinMode(pinLed2,OUTPUT);
+  Serial.begin(9600);
 }
 void loop()
 {
@@ -26,5 +38,13 @@ void loop()
       digitalWrite(ventilador,HIGH);
       }
     } 
+  /// apagar los leds del ldr al inicio
+  digitalWrite(pinLed1,LOW);
+  digitalWrite(pinLed2,LOW);
+  // guardamos el valor leido por el LDR}
+  valorLDR=analogRead(pinLDR);
+  // devolver el valor al monitor
+  Serial.println(valorLDR);
 }
+
 
