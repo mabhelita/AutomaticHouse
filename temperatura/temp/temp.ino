7f44ae23736f02e173c71351d910f46cd3860d5c
mabhelita <mabhelita@gmail.com>
2016-07-22 15:04:11 -0500

    control arduino ethernet

--- temperatura/temp/temp.ino
+++ temperatura/temp/temp.ino
@@ -1,3 +1,20 @@
+/*control del arduino mediante
+ ethernet 
+*/
+// librerias para ethernet
+#include<SPI.h>
+#include<Ethernet.h>
+#include<Servo.h>
+
+Servo microservo;
+int pos = 0;
+byte mac[] = { 0xA0, 0xB3, 0xCC, 0x87, 0xE7, 0x2E }; // direccion fisica MAC
+byte ip[] = { 10, 0, 0, 150 };                        // ip local que se debe configurar
+byte gateway[] = { 10, 0, 0, 1 };                     // puerta de enlace
+byte subnet[] = { 255, 0, 0, 0 };                      // mascara de sub red
+EthernetServer server(80);                            // se usa el puerto 80 del servidor
+String readString; 
+
 float tempC;
 int tempPin=0;
 int calefaccion=12;
@@ -11,6 +28,13 @@ int pinLDR=1;
 int valorLDR=0;
 void setup()
 {
+  // configurando ethernet
+  Ethernet.begin(mac, ip, gateway, subnet);  //inicializa la conexion ethernet y el servidor
+  server.begin();
+  Serial.print("el servidor es:");
+  Serial.println(Ethernet.localIP());    //imprime la direccion ip local
+
+  // configurando los pin de salida de la temperatura  
   Serial.begin(9600);
   pinMode(calefaccion,OUTPUT);
   pinMode(ventilador,OUTPUT);
@@ -22,6 +46,10 @@ void setup()
 }
 void loop()
 {
+  // conexion con la pagina
+
+  // condiciones para el encendido del calefactor y ventilador
+  
   tempC=analogRead(tempPin);
   tempC=(5.0*tempC*100.0)/1024.0;
   Serial.print(tempC);
